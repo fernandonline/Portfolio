7860e3216660fd581baec4dbe83831c79001666f
fernando <luizfernandosr@outlook.com.br>
2025-05-14 22:01:05 -0300

    Adicionar arquivos de projeto.

---
+++ Portfolio/Components/Header/Class.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+public class ThemeService
+{
+    private readonly IJSRuntime _js;
+
+    public ThemeService(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    public async Task SetTheme(string theme)
+    {
+        await _js.InvokeVoidAsync("setThemeAttribute", theme);
+    }
+}
