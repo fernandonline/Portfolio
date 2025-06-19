b4a37674870e9306f6d1233a0a33955c5473eb56
fernando <luizfernandosr@outlook.com.br>
2025-06-18 22:42:18 -0300

    animations

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
-using Portfolio.Components.DynamicHeader;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<ThemeService>();
 
 await builder.Build().RunAsync();
