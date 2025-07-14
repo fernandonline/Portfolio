27447db3f516e62f1698cf8c764c037a76a24536
fernando <luizfernandosr@outlook.com.br>
2025-07-14 20:16:15 -0300

    porjects-modal-1

---
+++ Portfolio/Layout/Projeto.cs
@@ -0,0 +1,14 @@
+namespace Portfolio.Layout
+{
+    public class Projeto
+    {
+        public int Id { get; set; }
+        public string? Img { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Tech { get; set; }
+        public string? GithubCode { get; set; }
+        public string? Deploy { get; set; }
+    }
+
+}
