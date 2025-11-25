0ba498803286fc4a5ccab35727de1b61dfc1db87
fernando <luizfernandosr@outlook.com.br>
2025-11-25 02:34:04 -0300

    github api

---
+++ Portfolio/Models/GitRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace Portfolio.Models
+{
+    public class GitRepo
+    {
+        public string name { get; set; }
+        public string html_url { get; set; }
+        public string description { get; set; }
+    }
+
+}
---
+++ Portfolio/Services/ProjetoService.cs
@@ -0,0 +1,23 @@
+﻿using Portfolio.Models;
+using System.Net.Http.Json;
+
+namespace Portfolio.Services
+{
+    public class ProjetoService
+    {
+        private readonly HttpClient _http;
+
+        private const string _url = "https://raw.githubusercontent.com/fernandonline/portfolio-data/main/projects.json";
+
+        public ProjetoService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<Projeto>> GetProjetosAsync()
+        {
+            var data = await _http.GetFromJsonAsync<List<Projeto>>(_url);
+            return data ?? [];
+        }
+    }
+}
